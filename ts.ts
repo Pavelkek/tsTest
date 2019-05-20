39fe1fabf6d74a739dab7266b41ee647599d3f52
Samotaev Pavel Andreevich <p.samotaev@tcsbank.ru>
2019-05-20 17:34:26 +0300

    needYourCheck

--- ts.ts
+++ ts.ts
@@ -1,5 +1,6 @@
 import {browser, element, by, protractor} from 'protractor';
-describe('Protractor Demo App', function() {
+describe('Type Script Test', function() {
+  let EC = protractor.ExpectedConditions;
   let geoButton = element(by.css('span.geolink__reg'));
   let town = element(by.css('input#city__front-input.input__control.input__input'));
   let moreButton = element(by.css("a.home-link.home-link_blue_yes.home-tabs__link.home-tabs__more-switcher.dropdown2__switcher"));
@@ -9,28 +10,31 @@ describe('Protractor Demo App', function() {
     await browser.get('https://yandex.ru/');
     console.log("before");
   });
-    it('should add one and two',async function() {
+    it('should compare more of London and Parish',async function() {
+      let isClickable = el => {
+        browser.wait(EC.elementToBeClickable(el), 5000);
+      }
       console.log("it");
+      isClickable(geoButton);
       await geoButton.click();
+      isClickable(town);
       await town.click();
       await town.clear();
       await town.sendKeys('Лондон');
-      browser.sleep(1000); //не заю, почему, но приходится браузер слипать, иначе интер не работает
-      await browser.actions().sendKeys(protractor.Key.ENTER).perform();
-      browser.sleep(1000);
+      browser.sleep(1000); //не знаю, как избавится от слипов перед интером
+      await town.sendKeys(protractor.Key.ENTER);
+      isClickable(moreButton);
       await moreButton.click();
-      browser.sleep(1000);
-      let londonMore = moreContent.getText();
+      let londonMore = await moreContent.getText();
       await geoButton.click();
       await town.click();
       await town.clear();
       await town.sendKeys('Париж');
-      browser.sleep(1000);
+      browser.sleep(1000); //не знаю, как избавится от слипов перед интером
       await browser.actions().sendKeys(protractor.Key.ENTER).perform();
-      browser.sleep(1000);
+      isClickable(moreButton);
       await moreButton.click();
-      browser.sleep(1000);
-      let parisMore = moreContent.getText();
+      let parisMore = await moreContent.getText();
       await expect(parisMore).toEqual(londonMore);
       console.log("success");
     });
--- typeScript/ts.js
+++ typeScript/ts.js
@@ -9,7 +9,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const protractor_1 = require("protractor");
-describe('Protractor Demo App', function () {
+describe('Type Script Test', function () {
+    let EC = protractor_1.protractor.ExpectedConditions;
     let geoButton = protractor_1.element(protractor_1.by.css('span.geolink__reg'));
     let town = protractor_1.element(protractor_1.by.css('input#city__front-input.input__control.input__input'));
     let moreButton = protractor_1.element(protractor_1.by.css("a.home-link.home-link_blue_yes.home-tabs__link.home-tabs__more-switcher.dropdown2__switcher"));
@@ -21,29 +22,32 @@ describe('Protractor Demo App', function () {
             console.log("before");
         });
     });
-    it('should add one and two', function () {
+    it('should compare more of London and Parish', function () {
         return __awaiter(this, void 0, void 0, function* () {
+            let isClickable = el => {
+                protractor_1.browser.wait(EC.elementToBeClickable(el), 5000);
+            };
             console.log("it");
+            isClickable(geoButton);
             yield geoButton.click();
+            isClickable(town);
             yield town.click();
             yield town.clear();
             yield town.sendKeys('Лондон');
-            protractor_1.browser.sleep(1000); //не заю, почему, но приходится браузер слипать, иначе интер не работает
-            yield protractor_1.browser.actions().sendKeys(protractor_1.protractor.Key.ENTER).perform();
-            protractor_1.browser.sleep(1000);
+            protractor_1.browser.sleep(1000); //не знаю, как избавится от слипов перед интером
+            yield town.sendKeys(protractor_1.protractor.Key.ENTER);
+            isClickable(moreButton);
             yield moreButton.click();
-            protractor_1.browser.sleep(1000);
-            let londonMore = moreContent.getText();
+            let londonMore = yield moreContent.getText();
             yield geoButton.click();
             yield town.click();
             yield town.clear();
             yield town.sendKeys('Париж');
-            protractor_1.browser.sleep(1000);
+            protractor_1.browser.sleep(1000); //не знаю, как избавится от слипов перед интером
             yield protractor_1.browser.actions().sendKeys(protractor_1.protractor.Key.ENTER).perform();
-            protractor_1.browser.sleep(1000);
+            isClickable(moreButton);
             yield moreButton.click();
-            protractor_1.browser.sleep(1000);
-            let parisMore = moreContent.getText();
+            let parisMore = yield moreContent.getText();
             yield expect(parisMore).toEqual(londonMore);
             console.log("success");
         });
